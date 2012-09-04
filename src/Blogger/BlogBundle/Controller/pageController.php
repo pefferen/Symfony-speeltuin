bf90a30ccebbf63d8aa76bfbf44d3acb42ce4fa7
Patrick van Efferen <patrickvanefferen@gmail.com>
2012-09-04 13:29:33 +0200

    About page toegevoegd, en links hiernaar op homepage toegevoegd.

--- src/Blogger/BlogBundle/Controller/pageController.php
+++ src/Blogger/BlogBundle/Controller/pageController.php
@@ -12,4 +12,9 @@ class PageController extends Controller
     return $this->render('BloggerBlogBundle:Page:index.html.twig');
   }
 
+  public function aboutAction()
+  {
+    Return $this->render('BloggerBlogBundle:Page:about.html.twig');
+  }
+
 }
