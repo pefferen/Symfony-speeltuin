9b02822e3435ec6513deb1332fa6b665511df242
Patrick van Efferen <patrickvanefferen@gmail.com>
2012-09-09 14:26:45 +0200

    Showing of comments works now, Migrations are still broken.

--- src/Blogger/BlogBundle/Controller/BlogController.php
+++ src/Blogger/BlogBundle/Controller/BlogController.php
@@ -24,8 +24,12 @@ class BlogController extends Controller
       throw $this->createNotFoundException('Unable to find blogpost.');
     }
 
+    $comments = $em->getRepository('BloggerBlogBundle:Comment')
+                   ->getCommentsForBlog($blog->getId());
+
     return $this->render('BloggerBlogBundle:Blog:show.html.twig', array(
-      'blog'  => $blog
+      'blog'      => $blog,
+      'comments'  => $comments
       ));
   }
 }
